ce838eb379b599b66bba53ea21690733fc247b6c
Daniil <d.bezlepkin@prosoftsystems.ru>
2023-11-06 14:29:47 +0500

    add(web) : added base web app, added .gitignore

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-import speech_recognition as sr
+from streamlit.web.cli import main
+import sys
 
-def command():
-	recognizer = sr.Recognizer()
-	with sr.Microphone() as source:
-		print("Слушаю голос:")
-		recognizer.adjust_for_ambient_noise(source, duration=1)
-		audio = recognizer.listen(source)
-	try:
-		text = recognizer.recognize_google(audio, language="ru-RU")
-		print(f"Вы сказали: \n {text}")
-	except sr.UnknownValueError:
-		print("Речь не распознана")
-		text = command()
 
 if __name__ == "__main__":
-	command()
-
-
-
+    sys.argv = ["streamlit", "run", "web.py", ""]
+    sys.exit(main(prog_name="streamlit"))
