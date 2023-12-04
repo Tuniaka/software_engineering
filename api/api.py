60feadacc66976ce96410313dd8f567f74c864f6
Daniil <d.bezlepkin@prosoftsystems.ru>
2023-12-04 20:54:14 +0500

    add(Docker,Makefile) : добавил докер образ для запуска API, добавил Makefile для быстрого развертывания на локальной машине

--- api/api.py
+++ api/api.py
@@ -1,10 +1,7 @@
 import speech_recognition as sr
-import pyperclip
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
-
 
 @app.get("/")
 async def read_root():
@@ -25,5 +22,3 @@ async def record_voice():
 		return {"error": f"Неизвестная ошибка: error"}
 
 
-if __name__ == "__main__":
-	uvicorn.run(app, host="127.0.0.1", port=8000)
