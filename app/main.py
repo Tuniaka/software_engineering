abf86bbc3528c0bdcf8555a721531f32e881e41a
Daniil <d.bezlepkin@prosoftsystems.ru>
2024-01-16 00:33:19 +0500

    fix(dev) : fix CI/CD

--- app/main.py
+++ app/main.py
@@ -3,5 +3,5 @@ import sys
 
 
 if __name__ == "__main__":
-    sys.argv = ["streamlit	", "run", "web.py", "--server.port=8000", "--server.address=0.0.0.0"]
+    sys.argv = ["streamlit	", "run", "web.py", "--server.port=8501", "--server.address=0.0.0.0"]
     sys.exit(main(prog_name="streamlit"))
